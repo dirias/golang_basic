6d879ff12a68c650b316106d25546aa0a463dfd7
Didier Irias Mendez <didier@Didier.localdomain>
2022-02-20 19:16:43 -0600

    Palyndrome done

--- src/main.go
+++ src/main.go
@@ -1,36 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
+func isPalyndrome(world string){
+	var textReverse string
+	
+	for i := len(world) -1; i >= 0; i--{
+		//si no se convierte a string directamente va a retornar el codigo ASCII
+		textReverse += string(world[i])
+	}
+	if strings.ToLower(world) == strings.ToLower(textReverse){
+		fmt.Println("Es palindromo")
+	}else{
+		fmt.Println("No es palindromo")
+
+	}
+
+}
 func main() {
-	fmt.Println("Working with arrays\n")
-	var array [5] int
 
-	array[0] = 1
-	array[1] = 2
-
-	fmt.Println(array, cap(array), len(array))
-
-	//slice
-	fmt.Println("\nWorking with slices\n")
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice,cap(slice), len(slice))
-	//El primer numero es inclusivo y el segundo exclusivo
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	//Los arrays son inmutables, sin embargo, se le pueden modificar los elementos ya existente.
-	//Los slices no mutables, se le pueden agregar o quitar elementos
-
-	//append - agregar un elemento nuevo
-	slice = append(slice, 7)
-	fmt.Println(slice,cap(slice), len(slice))
-
-	//append - agregar un otro slice
-	nuevoSlice := [] int {9, 10, 11}
-	slice = append(slice, nuevoSlice...)
-	fmt.Println(slice,cap(slice), len(slice))
+	world := "amor a romA"
+	
+	isPalyndrome(world)
 
 }
