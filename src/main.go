77b2e85b4c39dcec9643ceffeae98f05047f9b62
Didier Irias Mendez <didier@Didier.localdomain>
2022-02-21 09:24:16 -0600

    string method edited

--- src/main.go
+++ src/main.go
@@ -10,32 +10,13 @@ type pc struct{
 	brand string
 }
 
-func (myPC pc) ping(){
-	fmt.Println(myPC.brand, "Pong")
+func (myPC pc) String() string{
+	return fmt.Sprintf("Tengo %d GB Disco y es una %s", myPC.ram, myPC.disk, myPC.brand)
 }
 
-func (myPC *pc) duplicateRam(){
-	myPC.ram = myPC.ram * 2
-}
 func main() {
-	a := 50
-	b := &a
-	fmt.Println(b)
-	fmt.Println(*b)
-	*b = 100
-
-	fmt.Println(a)
 
 	myPC := pc{ram:16, disk:200, brand: "HP"}
 
 	fmt.Println(myPC)
-	myPC.duplicateRam()
-
-	fmt.Println(myPC)
-	myPC.duplicateRam()
-
-	fmt.Println(myPC)
-	myPC.duplicateRam()
-
-
 }
