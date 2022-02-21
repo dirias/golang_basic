f3871ae0efa6dcd77d349354d22ea59c5b303956
Didier Irias Mendez <didier@Didier.localdomain>
2022-02-21 09:17:16 -0600

    Punteros

--- src/main.go
+++ src/main.go
@@ -2,12 +2,40 @@ package main
 
 import (
 	"fmt"
-	"./mypackage"
 )
 
+type pc struct{
+	ram int
+	disk int
+	brand string
+}
+
+func (myPC pc) ping(){
+	fmt.Println(myPC.brand, "Pong")
+}
+
+func (myPC *pc) duplicateRam(){
+	myPC.ram = myPC.ram * 2
+}
 func main() {
-	var car mypackage.CarPublic
-	car.Brand = "Ferrari"
-	fmt.Println(car)
-	mypackage.PrintCar(car)
+	a := 50
+	b := &a
+	fmt.Println(b)
+	fmt.Println(*b)
+	*b = 100
+
+	fmt.Println(a)
+
+	myPC := pc{ram:16, disk:200, brand: "HP"}
+
+	fmt.Println(myPC)
+	myPC.duplicateRam()
+
+	fmt.Println(myPC)
+	myPC.duplicateRam()
+
+	fmt.Println(myPC)
+	myPC.duplicateRam()
+
+
 }
