66d56f8023e5580f6deb63bcfe9619254527a191
Didier Irias Mendez <didier@Didier.localdomain>
2022-02-20 20:24:06 -0600

    exporting packages

--- src/main.go
+++ src/main.go
@@ -2,22 +2,12 @@ package main
 
 import (
 	"fmt"
+	"./mypackage"
 )
-//Es la manera de crear clases en go
-type car struct{
-	brand string
-	year int
-}
 
 func main() {
-	//Manera 1 de instanciar clases
-	myCar := car{brand: "Ford", year: 2022}
-	fmt.Println(myCar)
-	//Manera 2 de instanciar clases
-	var otherCar car //Lo instancia como un constuctor vacio
-	otherCar.brand = "Ferrari" //Al no indicar el otro valor le asigna el nulo por defecto
-	fmt.Println(otherCar)
-
-
-
+	var car mypackage.CarPublic
+	car.Brand = "Ferrari"
+	fmt.Println(car)
+	mypackage.PrintCar(car)
 }
