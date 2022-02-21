c442d9f9e0c9ff20cf807994cfe6088582cd81f8
Didier Irias Mendez <didier@Didier.localdomain>
2022-02-21 10:11:35 -0600

    go routines

--- src/main.go
+++ src/main.go
@@ -1,40 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-type cuadrado struct {
-	base float64
-}
+func say(text string, wg *sync.WaitGroup) { // Gorutine
 
-type figuras2d interface {
-	area() float64
-}
+	defer wg.Done() // Esta linea se va a ejecutar hasta el final de la funcion, y de esta forma libera el gorutine del WaitGroup
 
-type rectangulo struct {
-	base   float64
-	altura float64
-}
-
-func (c cuadrado) area() float64 {
-	return c.base * c.base
-}
-
-func calcular(f figuras2d) {
-	fmt.Println("Area:", f.area())
-}
-
-func (r rectangulo) area() float64 {
-	return r.base * r.altura
+	fmt.Println(text)
 }
 
 func main() {
-	myCuadrado := cuadrado{base: 2}
-	myRectangulo := rectangulo{base: 2, altura: 4}
 
-	calcular(myCuadrado)
-	calcular(myRectangulo)
+	var wg sync.WaitGroup // El paquete sync permite interacturar de forma primitiva con las gorutine. Variable que acomula un conjunto de gorutines y los va liberando poco a poco
 
-	// Lista de interfaces
-	myInterface := []interface{}{"Hola", 12, 4.90}
-	fmt.Println(myInterface...)
+	fmt.Println("Hello")
+
+	wg.Add(1) // Indicamos que vamos a agregar 1 Gorutine al WaitGroup para que espere su ejecucion antes de que la gurutine base (main) muera, y así le de tiempo a la siguiente gorutine de ejecutarse
+
+	go say("world", &wg) // la palabra reservada go ejecutará la funcion de forma concurrente
+
+	wg.Wait() // Funcion del WaitGroup que sirve para decirle al gorutine principal (main) que espere hasta que todas las gorutine del WaitGroup finalicen, es decir, hasta que se ejecute 'defer wg.Done()' en cada una de las goroutines
+
+	go func(text string) { // Funciona anonima
+		fmt.Println(text)
+	}("Adios")
+
+	time.Sleep(time.Second * 1) // ! Funcion para que cuando llegue a esta linea espere el tiempo indicado (lo suficiente para que la Gorutine ejecute su funcion de forma concurrente)
+
+	// Nota: Para fines practicos se hace uso de la funcion Sleep(), pero en realidad NO es una buena practica, es mejor utilizar los WaitGroups
+
 }
